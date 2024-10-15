d992e5d2f8b8c14446ae3a7645c770ff8100a3b7
Вовк Алексей Анатольевич <avovk@samokat.ru>
2024-10-15 19:53:41 +0300

    add logic classes

---
+++ src/main/java/edu/praktikum/sprint6/Sail.java
@@ -0,0 +1,14 @@
+package edu.praktikum.sprint6;
+
+public class Sail {
+
+    private final int area;
+
+    public Sail(int area) {
+        this.area = area;
+    }
+
+    public int getMaxPower() {
+        return area * 3;
+    }
+}
